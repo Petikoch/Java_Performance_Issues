a4ac07cc7817d80faa47ecee5e506f5a6334d163
peti <petikoch@gmail.com>
2016-05-09 21:51:20 +0200

    fixed filename issue

--- src/main/java/ch/petikoch/examples/performance/Deadlock2Issue.java
+++ src/main/java/ch/petikoch/examples/performance/Deadlock2Issue.java
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * A typical Deadlock situation
  */
-public class DeadlockIssue {
+public class Deadlock2Issue {
 
 	public static void main(String[] args) {
 		System.out.println(ManagementFactory.getRuntimeMXBean().getName());
